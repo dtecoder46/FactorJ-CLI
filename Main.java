bc9329021bb114ba5e115016b4c4347eb9c5f4a7
Daniel Te <dte.biotech46@gmail.com>
2026-04-30 19:35:19 -0400

    Added condense() method

--- Main.java
+++ Main.java
@@ -48,10 +48,60 @@ public class Main {
 	public static ArrayList<Integer> factorize(int number) {
 		ArrayList<Integer> factorList = new ArrayList<Integer>();
 
-		// Base case: the chosen integer is a prime number, return itself and 1
-		factorList.add(1);
-		factorList.add(number);
+		if (number % 2 == 0) { // General case 1: even number
+			factorList.add(2);
+
+			int newNumber = number/2;
+			factorize(newNumber);
+		}
+
+		else if (number == 1) {
+			factorList = condense(factorList);
+		}
+
+		else {
+			// Base case: the chosen integer is a prime number, return itself and 1
+			factorList.add(1);
+			factorList.add(number);
+		}
+
 		return factorList;
+
+	}
+
+	/**
+	 * A method to condense the number of factors to 2
+	 * 
+	 * @param factors - the extended list of factors
+	 * @return factorsCondensed - the condensed list of factors
+	 */
+
+	public static ArrayList<Integer> condense(ArrayList<Integer> factors) {
+		// Define initial variables
+		int length = factors.size();
+		double middleIndex = length / 2.0;
+		int productOfLeft = 1;
+		int productOfRight = 1;
+		int index = 0;
+
+		// separate the list of factors using the middle index
+
+		while (index < length) {
+			if (index < middleIndex) {
+				productOfLeft *= factors.get(index);
+			}
+			else {
+				productOfRight *= factors.get(index);
+			}
+		}
+
+		// return a list containing productOfLeft and productOfRight
+		ArrayList<Integer> factorsCondensed = new ArrayList<Integer>();
+
+		factorsCondensed.add(productOfLeft);
+		factorsCondensed.add(productOfRight);
+
+		return factorsCondensed;
 	}
 
 	/**
